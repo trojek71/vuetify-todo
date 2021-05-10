dc071820aa779ae6983c90cdfe6a2f5f774c0f01
Tomek Rojek <tomekr@gmail.com>
2021-05-10 14:45:15 +0200

    add vuex done

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+        {
+          id: 1,
+          zadanie: "Wstawaj",
+          done: false,
+        },
+        {
+          id: 2,
+          zadanie: "zrób śniadanie",
+          done: false,
+        },
+        {
+          id: 3,
+          zadanie: "jedź do pracy",
+          done: false,
+        },
+      ],
   },
   mutations: {
+    addTask(state, newTaskTitle) {
+      let newTask = {
+        id: Date.now(),
+        zadanie: newTaskTitle,
+        done: false,
+      };
+      state.tasks.push(newTask);
+   
+    },
+    doneTask(state,id) {
+      let task = state.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+     deleteTask(state, id) {
+      state.tasks = state.tasks.filter((task) => task.id !== id);
+    },
   },
   actions: {
   },
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <v-text-field
       v-model="newTaskTitle"
-      @click:append="addTask()"
-      @keyup.enter="addTask()"
+      @click:append="addTask"
+      @keyup.enter="addTask"
       class="pa-3"
       outlined
       label="Dodaj zadanie"
@@ -11,10 +11,10 @@
       hide-details
       clearable
     ></v-text-field>
-    <v-list v-if="tasks.length" class="pt-0" flat>
-      <div v-for="task in tasks" :key="task.id">
+    <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
+      <div v-for="task in $store.state.tasks" :key="task.id">
         <v-list-item
-          @click="doneTask(task.id)"
+          @click="$store.commit('doneTask', task.id)"
           :class="{ 'blue lighten-5': task.done }"
         >
           <template v-slot:default>
@@ -29,7 +29,7 @@
               >
             </v-list-item-content>
             <v-list-item-action>
-              <v-btn icon @click.stop="deleteTask(task.id)">
+              <v-btn icon @click.stop="$store.commit('deleteTask', task.id)">
                 <v-icon color="primary">mdi-delete</v-icon>
               </v-btn>
             </v-list-item-action>
@@ -51,41 +51,12 @@ export default {
   data() {
     return {
       newTaskTitle: "",
-      tasks: [
-        {
-          id: 1,
-          zadanie: "Wstawaj",
-          done: false,
-        },
-        {
-          id: 2,
-          zadanie: "zrób śnadanie",
-          done: false,
-        },
-        {
-          id: 3,
-          zadanie: "jedź do pracy",
-          done: false,
-        },
-      ],
     };
   },
   methods: {
     addTask() {
-      let newTask = {
-        id: Date.now(),
-        zadanie: this.newTaskTitle,
-        done: false,
-      };
-      this.tasks.push(newTask);
+      this.$store.commit("addTask", this.newTaskTitle);
       this.newTaskTitle = "";
-    },
-    doneTask(id) {
-      let task = this.tasks.filter((task) => task.id === id)[0];
-      task.done = !task.done;
-    },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
