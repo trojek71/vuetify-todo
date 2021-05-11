a252b5a2b7b20643a422de1cae2243f2eb9c3e1c
Tomek Rojek <tomekr@gmail.com>
2021-05-11 13:29:06 +0200

    sekcja  6 DONE

---
+++ src/components/Todo/NoTask.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="no-tasks">
+    <v-icon class="pa-6" size="100" color="primary">mdi-check</v-icon>
+    <div class="pa-7 text-h5 primary--text">Brak Zadań</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+.no-tasks
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+    opacity: 50%
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,71 +1,23 @@
 <template>
   <div class="home">
-    <v-text-field
-      v-model="newTaskTitle"
-      @click:append="addTask"
-      @keyup.enter="addTask"
-      class="pa-3"
-      outlined
-      label="Dodaj zadanie"
-      append-icon="mdi-plus"
-      hide-details
-      clearable
-    ></v-text-field>
-    <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.zadanie }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn icon @click.stop="$store.commit('deleteTask', task.id)">
-                <v-icon color="primary">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
-    <div v-else class="no-tasks">
-      <v-icon class="pa-6" size="100" color="primary">mdi-check</v-icon>
-      <div class="pa-7 text-h5 primary--text">Brak Zadań</div>
-    </div>
+    <field-and-task />
+    <list-tasks v-if="$store.state.tasks.length" />
+    <no-tasks v-else />
   </div>
 </template>
 
 <script>
+import ListTasks from "../components/Todo/ListTasks.vue";
+import NoTask from "../components/Todo/NoTask.vue";
 export default {
   name: "Home",
-  data() {
-    return {
-      newTaskTitle: "",
-    };
-  },
-  methods: {
-    addTask() {
-      this.$store.commit("addTask", this.newTaskTitle);
-      this.newTaskTitle = "";
-    },
+
+  components: {
+    "field-and-task": require("@/components/Todo/FieldAndTask.vue").default,
+    "list-tasks": require("@/components/Todo/ListTasks.vue").default,
+    "no-tasks": require("@/components/Todo/NoTask.vue").default,
   },
 };
 </script>
-<style lang="sass">
-.no-tasks
-  position: absolute
-  left: 50%
-  top: 50%
-  transform: translate(-50%, -50%)
-  opacity: 50%
+<style >
 </style>
